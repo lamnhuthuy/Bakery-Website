7205603828ee8e815e4752fac6d32cb0459579ae
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-08 10:49:54 +0700

    Feat: Create view Cart.php

---
+++ App/Views/cart/cart.php
@@ -0,0 +1,357 @@
+<div class="container">
+  <h3 class="detail-title">Cart</h3>
+  <div class="cart-control">
+    <div class="cart1">
+      <div id="1" class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+      <div class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+      <div class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+      <div class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+      <div class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+      <div class="cart1-conatain">
+        <img src="img/cakes/1.1.jpg" alt="" class="cart1-conatain-img" />
+        <div class="cart-detail">
+          <p class="cart-detail-name">Blue bery Mousse</p>
+          <p class="cart-detail-name">Size: 20 cm</p>
+          <p class="cart-detail-name cart-detail-name-color">
+            Price: 200.000 VND
+          </p>
+          <label for="amount">Quantity:</label>
+          <input type="number" name="" value="1" id="amount" min="1" max="99" />
+        </div>
+        <div class="cart1-delete">
+          <svg
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="30px"
+            height="30px"
+            viewBox="0 0 92.132 92.132"
+            style="enable-background: new 0 0 92.132 92.132"
+            xml:space="preserve"
+          >
+            <g>
+              <g>
+                <path
+                  fill="#f3455a"
+                  d="M2.141,89.13c1.425,1.429,3.299,2.142,5.167,2.142c1.869,0,3.742-0.713,5.167-2.142l33.591-33.592L79.657,89.13
+    c1.426,1.429,3.299,2.142,5.167,2.142c1.867,0,3.74-0.713,5.167-2.142c2.854-2.854,2.854-7.48,0-10.334L56.398,45.205
+    l31.869-31.869c2.855-2.853,2.855-7.481,0-10.334c-2.853-2.855-7.479-2.855-10.334,0L46.065,34.87L14.198,3.001
+    c-2.854-2.855-7.481-2.855-10.333,0c-2.855,2.853-2.855,7.481,0,10.334l31.868,31.869L2.143,78.795
+    C-0.714,81.648-0.714,86.274,2.141,89.13z"
+                ></path>
+              </g>
+            </g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+            <g></g>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="cart2">
+      <p class="cart2-title">Delivery Information</p>
+      <div class="cart2-user">
+        <p class="cart2-user-name">User name:</p>
+        <p>Lam Nhut Huy.</p>
+      </div>
+      <div class="cart2-user">
+        <p class="cart2-user-name">Phone:</p>
+        <p>0799543369.</p>
+      </div>
+      <p class="cart2-user-name cart2-user-name-margin">Address:</p>
+      <p class="cart2-user-name cart2-user-name-margin">
+        KTX B, Dai hoc Can Tho.
+      </p>
+      <p class="cart2-user-name cart2-user-name-margin">
+        Total:
+        <span id="total">200.000 VND</span>
+      </p>
+      <button class="btn btn--primary">Check out</button>
+    </div>
+  </div>
+</div>
