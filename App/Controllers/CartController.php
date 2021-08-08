2047df26634c13046eed698564f3d499c966228f
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-08 22:09:39 +0700

    Feat: Update CartController.
    - Add method updateCart.
    - Add method deleteCart.
    - Add  and update method index.

--- App/Controllers/CartController.php
+++ App/Controllers/CartController.php
@@ -5,9 +5,11 @@ use App\Core\Controller;
 class CartController extends Controller
 {
     private $cartModel;
+    private $cakeModel;
     function __construct()
     {
         $this->cartModel = $this->model("CartModel");
+        $this->cakeModel = $this->model("CakeModel");
     }
     function addToCart()
     {
@@ -25,9 +27,38 @@ class CartController extends Controller
     }
     function index()
     {
-        $result = $this->cartModel->check();
-        foreach ($result as $i => $cart) {
-            print_r($cart);
+        if (isset($_SESSION["user"])) {
+            $Cake = $this->cartModel->getCakeOfUser($_SESSION['user']['id']);
+            $data["amount"] = $this->cartModel->amountInCart($_SESSION['user']['id']);
+            $data["total"] = 0;
+            if ($Cake == false) {
+                $data["cake"][] = [];
+            } else {
+                foreach ($Cake as $index => $value) {
+                    $data["cake"][] = $this->cakeModel->getCakeByID($value["id_cake"]);
+                    $data["amountCake"][] = $value["amount"];
+                    $data["total"] += $value["amount"] * $data["cake"][$index]["price"];
+                }
+            }
+            $this->view("/cart/cart", $data);
+        } else {
+            $this->view("/cart/cart");
+        }
+    }
+    function updateCart()
+    {
+        if (isset($_POST)) {
+            $result = $this->cartModel->updateCart($_POST);
+            echo json_encode($result);
+        }
+    }
+    function deleteCart()
+    {
+        if (isset($_POST)) {
+            $result = $this->cartModel->deleteCart($_POST);
+            if ($result == true) {
+                echo json_encode($this->cartModel->amountInCart($_POST["id_user"]));
+            }
         }
     }
 }
