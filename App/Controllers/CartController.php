d7c71127c1faabdfbb5f8093a925e453035df66d
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-09 17:54:43 +0700

    Feat: Update CartController.
    - Add method checkOut.

--- App/Controllers/CartController.php
+++ App/Controllers/CartController.php
@@ -10,6 +10,7 @@ class CartController extends Controller
     {
         $this->cartModel = $this->model("CartModel");
         $this->cakeModel = $this->model("CakeModel");
+        $this->orderModel = $this->model("OrderModel");
     }
     function addToCart()
     {
@@ -59,6 +60,26 @@ class CartController extends Controller
             if ($result == true) {
                 echo json_encode($this->cartModel->amountInCart($_POST["id_user"]));
             }
+        }
+    }
+    function checkOut()
+    {
+
+        if ($this->cartModel->amountInCart($_SESSION['user']['id']) < 1) {
+            $_SESSION["info"] = "no";
+            header("Location: " . DOCUMENT_ROOT);
+            return;
+        } else {
+            $allCake = $this->cartModel->getCakeOfUser($_SESSION['user']['id']);
+            $data["total"] = 0;
+            foreach ($allCake as $index => $value) {
+                $data["cake"][] = $this->cakeModel->getCakeByID($value["id_cake"]);
+                $data["amountCake"][] = $value["amount"];
+                $data["total"] += $value["amount"] * $data["cake"][$index]["price"];
+            }
+            $this->orderModel->orderProcess($data);
+            $_SESSION["info"] = "yes";
+            header("Location: " . DOCUMENT_ROOT);
         }
     }
 }
