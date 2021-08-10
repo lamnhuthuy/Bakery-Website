1933476eb7c30160343136ffa6f929a20c927bf6
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-10 13:31:27 +0700

    Feat: Update ProfileController

--- App/Controllers/ProfileController.php
+++ App/Controllers/ProfileController.php
@@ -18,15 +18,18 @@ class ProfileController extends Controller
         if (isset($_SESSION["user"])) {
             $data["user"] = $this->userModel->getById($_SESSION["user"]["id"]);
             $data["order"] = $this->orderModel->getById($_SESSION["user"]["id"]);
-            foreach ($data["order"] as $index => $value) {
-                $data["order_detail"][$value["id"]] = $this->orderModel->getOrderDetail($value["id"]);
-                foreach ($data["order_detail"][$value["id"]] as $count => $cake) {
-                    $idCake = $cake["id_cake"];
-                    $data["cake_detail"][$value["id"]][$idCake] = $this->cakeModel->getCakeByID($idCake);
+            if ($data["order"] != []) {
+                foreach ($data["order"] as $index => $value) {
+                    $data["order_detail"][$value["id"]] = $this->orderModel->getOrderDetail($value["id"]);
+                    foreach ($data["order_detail"][$value["id"]] as $count => $cake) {
+                        $idCake = $cake["id_cake"];
+                        $data["cake_detail"][$value["id"]][$idCake] = $this->cakeModel->getCakeByID($idCake);
+                    }
+                    $data["status"][$value["id"]] = $this->orderModel->getStatus($value["id_status"]);
                 }
-                $data["status"][$value["id"]] = $this->orderModel->getStatus($value["id_status"]);
+            } else {
+                $data["order"] = [];
             }
-
             $this->view("/profile/profile", $data);
         }
     }
