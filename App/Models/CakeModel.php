aa98cdf41982e683489042b043a2691eb25dadd8
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-15 16:58:30 +0700

    Feat: Update CakeModel
    - Add method getComment of user.
    -Add method addComment to add a comment from user.
    - Add method updateComment(update likes field) and update thich table.
    - add method updatethich to select row user like in table thich

--- App/Models/CakeModel.php
+++ App/Models/CakeModel.php
@@ -45,4 +45,54 @@ class CakeModel extends Database
             return $result->fetch_assoc();
         } else return;
     }
+    function getComments($id)
+    {
+        $sql = "Select * from comments where id_cake= ? ";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } else return;
+    }
+    function addComment($id, $time, $comment)
+    {
+        $id_cake = $id;
+        $id_user = $_SESSION["user"]["id"];
+        $like = 0;
+        $sql = "Insert into Comments(id_cake,id_user,comment,time,likes) values(?,?,?,?,?) ";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("iissi", $id_cake, $id_user, $comment,  $time, $like);
+        $result = $stmt->execute();
+        return   true;
+    }
+    function updateComment($like, $comment, $user)
+    {
+        $stmt = $this->con->prepare("UPDATE Comments SET likes = ? WHERE id = ? ");
+        $stmt->bind_param("ii", $like, $comment);
+        $stmt->execute();
+        $kq = $this->updateThich($user, $comment);
+        if ($kq == 1) {
+            $stmt = $this->con->prepare("DELETE FROM Thich WHERE id_comment = ? AND id_user = ?");
+            $stmt->bind_param("ii",  $comment, $user);
+            $stmt->execute();
+        } else {
+            $sql = "Insert into thich(id_comment,id_user) values(?,?) ";
+            $stmt = $this->con->prepare($sql);
+            $stmt->bind_param("ii",  $comment, $user);
+            $stmt->execute();
+        }
+    }
+    function updateThich($id, $comment)
+    {
+        $sql = "Select * from thich where id_user= ? and id_comment=?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("ii", $id, $comment);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return $result->num_rows;
+        } else return 0;
+    }
 }
