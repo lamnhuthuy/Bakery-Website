615ec91cd31e14c5ba2a77bf653f6b7079a3777f
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-17 18:39:56 +0700

    Feat: Update category Model
    - Add method getName.
    - Add method getImage.
    - Add method getById_cake_type

--- App/Models/CategoryModel.php
+++ App/Models/CategoryModel.php
@@ -12,4 +12,37 @@ class CategoryModel extends Database
             return $result->fetch_all(MYSQLI_ASSOC);
         } else return;
     }
+    function getNameById($id)
+    {
+        $sql = "SELECT * FROM cake_type where id=?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc()["name"];
+        } else return;
+    }
+    function getImage($id)
+    {
+        $sql = "SELECT * FROM images where id_cake=?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } else return;
+    }
+    function getCakeByID_type($id_type)
+    {
+        $sql = "SELECT * FROM cakes where id_cake_type=?";
+        $stmt = $this->con->prepare($sql);
+        $stmt->bind_param("i", $id_type);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            return $result->fetch_all(MYSQLI_ASSOC);
+        } else return;
+    }
 }
