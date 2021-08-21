7180e795105c4744b462ca765d7a6258f44fdd18
lamnhuthuy <86046511+lamnhuthuy@users.noreply.github.com>
2021-08-21 11:25:08 +0700

    Feat: Update index method

--- App/Controllers/CategoryController.php
+++ App/Controllers/CategoryController.php
@@ -12,8 +12,14 @@ class CategoryController extends Controller
     }
     function index()
     {
+        $data["type"] = $_GET["type"];
         $data["cake"] = $this->categoryModel->getCakeByID_type($_GET["type"]);
         $data["name"] = $this->categoryModel->getNameById($_GET["type"]);
+        if (isset($_GET["sort"])) {
+            $data["cake"] = $this->categoryModel->sort($_GET["type"], $_GET["sort"]);
+            $this->view("/cake/category", $data);
+            return;
+        }
         $this->view("/cake/category", $data);
     }
 }
